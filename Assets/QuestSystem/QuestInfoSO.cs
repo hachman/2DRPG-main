7b81155403e19448786b529f51289d5760d9908f
Sophearia <40466446+hachman@users.noreply.github.com>
2024-02-26 13:33:51 +0800

    update

--- Assets/QuestSystem/QuestInfoSO.cs
+++ Assets/QuestSystem/QuestInfoSO.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[CreateAssetMenu(fileName = "QuestInfoSo", menuName ="ScriptableObjects/QuestInfoSO", order = 1)]
+public class QuestInfoSo : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: SerializeField] public string id {  get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    [Header("General")]
+
+    public string displayName;
+
+    [Header("Requirements")]
+    
+    public int levelRequirment;
+
+    public QuestInfoSo[] questPrerequisites;
+
+    [Header("Steps")]
+
+    public GameObject[] questStepPrefabs;
+
+    [Header("Rewards")]
+
+    public int KnowledgeReward;
+
+    public int experienceReward;
+    private void OnValidate()
     {
-        
+#if UNITY_EDITOR
+        id = this.name;
+        UnityEditor.EditorUtility.SetDirty(this);
+#endif
     }
 }
