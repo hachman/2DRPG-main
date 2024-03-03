bfe80f18c7c49b6e5eb355c8c4c2ef2c8dadc9b0
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-03 13:41:48 +0800

    upscale

--- Assets/Script/Level Manager/ToPrevScene.cs
+++ Assets/Script/Level Manager/ToPrevScene.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToPrevScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     private int prevSceneToLoad;
+
+    private void Start()
     {
-        
+        prevSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        SceneManager.LoadScene(prevSceneToLoad);
     }
 }
