5a85d7c2259eed6a2b1903c4708ee9c17fab5369
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-03 12:00:23 +0800

    new script

---
+++ Assets/Script/SAVE SYSTEM/SAVESYSTEM.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SAVESYSTEM : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetString("Input", inputField.text);
+    }
+
+    public void LoadData()
+    {
+      inputField.text =  PlayerPrefs.GetString("Input");
+    }
+
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteKey("Input");
+    }
+}
