70ec7542cbc94a191eeb9f425075f6b4b0a56db8
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-03 16:41:22 +0800

    Update DialogueManagerHelpers1.cs

--- Assets/Script/Dialogue/DialogueManagerHelpers1.cs
+++ Assets/Script/Dialogue/DialogueManagerHelpers1.cs
@@ -1,12 +1,1 @@
-﻿internal static class DialogueManagerHelpers1
-{
-
-    private static DialogueManager instance;
-    private static DialogManager Instance;
-
-    public static DialogManager GetInstance()
-    {
-
-        return instance;
-    }
-}
+﻿
