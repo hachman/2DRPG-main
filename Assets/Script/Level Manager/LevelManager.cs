2bfe62d3116472baa84d3fdc0e221c927c5f74bc
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-03 13:01:00 +0800

    Update LevelManager.cs

--- Assets/Script/Level Manager/LevelManager.cs
+++ Assets/Script/Level Manager/LevelManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
 
     [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
     private void Awake()
     {
         if (instance == null)
@@ -21,9 +25,19 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    public async void loadScene(string sceneName)
+    public async void LoadScene(string sceneName)
     {
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+           // _progressBar.fillAmount = scene.progress;
+            
+
+        } while (useGUILayout);
     }
 }
