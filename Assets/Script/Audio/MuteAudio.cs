a5325b77a82b927b2a517b5ba3aaccb933d133a2
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-16 11:51:06 +0800

    MainMenu Update

---
+++ Assets/Script/Audio/MuteAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MuteAudio : MonoBehaviour
+{
+    public void MuteHandler(bool mute) {
+        if (mute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
+}
