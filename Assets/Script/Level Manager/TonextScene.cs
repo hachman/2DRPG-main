e02170b89203ad02b98cd6d71e900d268500d8e3
Sophearia <40466446+hachman@users.noreply.github.com>
2024-03-18 09:11:31 +0800

    up

--- Assets/Script/Level Manager/TonextScene.cs
+++ Assets/Script/Level Manager/TonextScene.cs
@@ -6,14 +6,24 @@ using UnityEngine.SceneManagement;
 public class TonextScene : MonoBehaviour
 {
     private int nextSceneToLoad;
+    FadeInOut fade;
 
     private void Start()
-    {
+    { 
+        fade = FindAnyObjectByType<FadeInOut>();
         nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public IEnumerator _Changescene()
+    {
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(nextSceneToLoad);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(nextSceneToLoad);
+        StartCoroutine(_Changescene());
+        
     }
 }
